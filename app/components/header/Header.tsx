7e26a51ff546b14b210d81434a029bfd9e67cef2
egcgt3 <egcgt3@gmail.com>
2026-09-22 16:42:58 -0700

    feat: link header to home

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,7 +1,11 @@
+import  Link from "next/link";
+
 export default function Header() {
   return (
     <div className="navbar bg-base-100 shadow-sm">
-      <a className="btn btn-ghost text-xl">Gerardo&apos;s Car Dealership</a>
+      <Link href="/" className="btn btn-ghost text-xl">
+        Gerardo&apos;s Car Dealership
+      </Link>
     </div>
   );
 }
